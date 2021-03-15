5b507ae1872cf4049d7821f7e07fa24731e4b434
Ayvi95 <80670970+Ayvi95@users.noreply.github.com>
2021-03-15 00:09:29 -0700

    Add files via upload

---
+++ feathers2.py
@@ -0,0 +1,146 @@
+# OLED Code, code for the display
+
+import time
+import busio
+import board
+import displayio
+import terminalio
+import adafruit_displayio_sh1107
+from adafruit_display_text import label
+
+displayio.release_displays()
+# oled_reset = board.D9
+
+# Use for I2C
+i2c = board.I2C()
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+
+# SH1107
+WIDTH = 128
+HEIGHT = 64
+BORDER = 1
+
+display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
+
+# Display Context
+splash = displayio.Group(max_size=10)
+display.show(splash)
+
+color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFFFFFF  # White
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+# Border
+inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0x000000  # Black
+inner_sprite = displayio.TileGrid(
+    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
+)
+splash.append(inner_sprite)
+
+# Title Lower Border
+sm_bitmap = displayio.Bitmap(128, 1, 30)
+sm_square = displayio.TileGrid(sm_bitmap, pixel_shader=color_palette, x=1, y=17)
+splash.append(sm_square)
+
+# Title and Text
+text1 = "Humidity & Temp"
+text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=5, y=8)
+splash.append(text_area)
+text2 = "(sensor info)"
+text_area2 = label.Label(
+    terminalio.FONT, text=text2, scale=1, color=0xFFFFFF, x=25, y=38
+)
+splash.append(text_area2)
+
+while True:
+    pass
+
+# HTS Code, code for the humidity and temperature sensor
+"""
+import time
+import busio
+import board
+import adafruit_shtc3
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sht = adafruit_shtc3.SHTC3(i2c)
+
+while True:
+    temperature, relative_humidity = sht.measurements
+    print("Temperature: %0.1f C" % temperature)
+    print("Humidity: %0.1f %%" % relative_humidity)
+    print("")
+    time.sleep(1)
+"""
+# PAS Code, code for the pressure and altitude sensor
+"""
+import time
+import board
+import busio
+import adafruit_bmp280
+
+i2c = busio.I2C(board.SCL, board.SDA)
+bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+
+bmp280.sea_level_pressure = 1013.25
+
+while True:
+    print("Pressure: %0.1f hPa" % bmp280.pressure)
+    print("Altitude = %0.2f meters" % bmp280.altitude)
+    time.sleep(2)
+"""
+# AQS Code, code for the air quality sensor
+"""
+# pylint: disable=unused-import
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+
+reset_pin = None
+
+# Create library object, use 'slow' 100KHz frequency!
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+pm25 = PM25_I2C(i2c, reset_pin)
+
+print("Found PM2.5 sensor, reading data...")
+
+while True:
+    time.sleep(1)
+
+    try:
+        aqdata = pm25.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+
+    print()
+    print("Concentration Units (standard)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
+    print("Concentration Units (environmental)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
+    print("---------------------------------------")
+    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    print("---------------------------------------")
+"""
